3b76e6420767c628c019b5df347149bc178c3fa5
cnlohr <lohr85@gmail.com>
2021-10-05 02:36:58 -0700

    Update!

---
+++ Assets/h264fun/H264funButton.cs
@@ -0,0 +1,54 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDK3.Components.Video;
+using VRC.SDK3.Video.Components;
+using VRC.SDK3.Video.Components.AVPro;
+using VRC.SDK3.Video.Components.Base;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRC.Udon.Common;
+
+using UnityEngine.UI;
+
+public class H264funButton : UdonSharpBehaviour
+{
+	public VRCUnityVideoPlayer unityVideo;
+	public Text textOut;
+	private int interact_count;
+    public VRCUrl _videoURL;
+
+    void Start()
+    {		
+		if (Utilities.IsValid(unityVideo))
+		{
+			unityVideo.Loop = false;
+			unityVideo.Stop();
+			unityVideo.EnableAutomaticResync = false;
+		}
+    }
+	
+	public override void Interact()
+	{
+		interact_count++;
+		Debug.Log( "Play!" );
+		if( unityVideo.IsPlaying )
+		{
+			unityVideo.Stop();
+		}
+		else
+		{
+#if !UNITY_EDITOR
+			unityVideo.LoadURL( _videoURL );
+#endif
+			unityVideo.Play();
+		}
+	}
+	
+	void Update()
+	{
+		float time = unityVideo.GetTime();
+		textOut.text = $"Unity Video\nTime: {time}\nPlaying: {unityVideo.IsPlaying}\nCount: {interact_count}";
+	}
+	
+}
